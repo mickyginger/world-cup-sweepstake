f60ba6520c4c6bb82d9327e1736b2ba2ff6a7003
mickyginger <mickyginger@gmail.com>
2018-06-16 20:02:03 +0100

    Added basic error handling

--- controllers/registrations.js
+++ controllers/registrations.js
@@ -4,15 +4,31 @@ function newRoute(req, res) {
   res.render('register');
 }
 
-function createRoute(req, res) {
+function createRoute(req, res, next) {
   User.create(req.body)
-    .then(() => res.redirect('/'));
+    .then(() => {
+      req.flash('success', 'Thanks for registering.');
+      res.redirect('/');
+    })
+    .catch(err => {
+      req.flash('danger', 'Snap! Something wen\'t wrong. Please try again.');
+      res.redirect('/');
+      next(err);
+    });
 }
 
-function deleteRoute(req, res) {
+function deleteRoute(req, res, next) {
   User.findById(req.params.id)
     .then(user => user.remove())
-    .then(() => res.redirect('/admin'));
+    .then(() => {
+      req.flash('success', 'User successfully deleted.');
+      res.redirect('/admin');
+    })
+    .catch(err => {
+      req.flash('danger', 'Snap! Something wen\'t wrong. Please try again.');
+      res.redirect('/admin');
+      next(err);
+    });
 }
 
 module.exports = {
